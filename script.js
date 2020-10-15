0aa26a65533a6cf30a208033e1219ae76bb18d60
benjaminwcouch <72532063+benjaminwcouch@users.noreply.github.com>
2020-10-15 21:46:05 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function myFunction() {
    
   window.open("https://benjaminwcouch.github.io/netflix")
 }
+
+function myFunction2() {
+   
+  window.open("https://benjaminwcouch.github.io/weather")
+}
